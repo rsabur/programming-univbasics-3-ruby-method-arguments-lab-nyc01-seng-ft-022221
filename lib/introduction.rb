90e95ea6bc7cf894e5d520c38dedcb929797cb0d
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-22 22:43:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction_with_language(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{landuage}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 
 end
 
